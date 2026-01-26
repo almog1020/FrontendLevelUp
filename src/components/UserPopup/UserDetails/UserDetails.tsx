1d4ca68a8e8c4c0847cec59e58a0dfbfa0f68698
benzybert-gni <ben@g-ni.com>
2026-01-26 12:48:45 +0200

    conflicts

--- src/components/UserPopup/UserDetails/UserDetails.tsx
+++ src/components/UserPopup/UserDetails/UserDetails.tsx
@@ -3,8 +3,11 @@ import styles from './UserDetails.module.scss'
 import dashboardIcon from '../../../assets/dashboard.png'
 import profileIcon from '../../../assets/profile.png'
 import logoutIcon from '../../../assets/logout.png'
+import {useNavigate} from "react-router-dom";
 
 export function UserDetails({name,role,onLogout}:{name:string, role:UserRole,onLogout():void}) {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.user_menu}>
             <div className={styles.user_info}>
@@ -12,7 +15,7 @@ export function UserDetails({name,role,onLogout}:{name:string, role:UserRole,onL
                 <div className={styles.user_role}>{role}</div>
             </div>
             <div className={styles.menu_items}>
-                <button className={styles.menu_item}>
+                <button className={styles.menu_item} onClick={() => navigate('/user')}>
                     <img src={dashboardIcon} alt="Dashboard" className={styles.menu_item_icon}/>
                     <div className={styles.title}>Dashboard</div>
                 </button>
