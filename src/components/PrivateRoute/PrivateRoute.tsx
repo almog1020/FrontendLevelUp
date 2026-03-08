3a81c649a3968a80888c90c38df24d9f9d846437
almog1020 <102291459+almog1020@users.noreply.github.com>
2026-03-08 22:06:37 +0200

    Add useContext import in PrivateRoute component

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -2,7 +2,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import {useCookies} from "react-cookie";
 import {AuthContext} from "../AuthProvider/AuthProvider.tsx";
-
+import {useContext} from 'react';
 const PrivateRoute = () => {
     const [cookies] = useCookies()
     const auth = useContext(AuthContext);
